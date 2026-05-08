844f822e1d3095fdeddb686acbed98467aecdd02
Bryan <24001655@myrp.edu.sg>
2026-05-08 16:44:54 +0800

    app.py updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
