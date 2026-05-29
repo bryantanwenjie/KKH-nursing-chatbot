81c7a68d9ee2bd95088cc9fc64fc469c01cb3ea8
Bryan <24001655@myrp.edu.sg>
2026-05-29 14:37:41 +0800

    app.py updated

--- app.py
+++ app.py
@@ -128,9 +128,9 @@ def login_user(email, password):
 
         if row:
             return True, {
-                "user_id": row,
-                "full_name": row,
-                "email": row
+                "user_id": row,    # Grabs the 1st column (user_id)
+                "full_name": row,  # Grabs the 2nd column (full_name)
+                "email": row       # Grabs the 3rd column (email)
             }
         return False, None
 
